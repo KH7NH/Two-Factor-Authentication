bd0a87b9c8657cd65c4150b6629591ab3b51344e
Admin <admin@MacBook-Air-cua-Admin.local>
2025-05-29 15:25:45 +0700

    Call API 2FA format QRCode

--- react-web/src/components/setup-2fa.jsx
+++ react-web/src/components/setup-2fa.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { toast } from 'react-toastify'
 import Box from '@mui/material/Box'
 import Modal from '@mui/material/Modal'
@@ -7,11 +7,21 @@ import SecurityIcon from '@mui/icons-material/Security'
 import CancelIcon from '@mui/icons-material/Cancel'
 import TextField from '@mui/material/TextField'
 import Button from '@mui/material/Button'
+import { get2FA_QRCodeAPI } from '~/apis'
 
 // Tài liệu về Material Modal rất dễ ở đây: https://mui.com/material-ui/react-modal/
-function Setup2FA({ isOpen, toggleOpen }) {
+function Setup2FA({ isOpen, toggleOpen, user }) {
   const [otpToken, setConfirmOtpToken] = useState('')
   const [error, setError] = useState(null)
+  const [qrCodeImageUrl, setQrCodeImageUrl] = useState(null)
+
+  useEffect(() => {
+    if(isOpen){
+      get2FA_QRCodeAPI(user._id).then(res => {
+        setQrCodeImageUrl(res.qrcode)
+      })
+    }
+  }, [isOpen, user._id])
 
   const handleCloseModal = () => {
     toggleOpen(!isOpen)
@@ -61,11 +71,14 @@ function Setup2FA({ isOpen, toggleOpen }) {
         </Box>
 
         <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', gap: 2, p: 1 }}>
-          <img
-            style={{ width: '100%', maxWidth: '250px', objectFit: 'contain' }}
-            src="src/assets/trungquandev-qr-code.png"
-            alt="card-cover"
-          />
+          {!qrCodeImageUrl
+          ? <span>Loading QR Code...</span>
+            : <img
+              style={{ width: '100%', maxWidth: '250px', objectFit: 'contain' }}
+              src={qrCodeImageUrl}
+              alt="card-2fa"
+            />
+          }
 
           <Box sx={{ textAlign: 'center' }}>
             Quét mã QR trên ứng dụng <strong>Google Authenticator</strong> hoặc <strong>Authy</strong> của bạn.<br />Sau đó nhập mã gồm 6 chữ số và click vào <strong>Confirm</strong> để xác nhận.
